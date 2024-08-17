abae60da55d7cb0265b4c8922aa8cb7eb48ee780
BUITHITUYETNGOC <buithituyetngoc2402@gmail.com>
2024-08-17 10:35:49 +0700

    Update Admin and delete the temp file

--- BOOKSTORE/Areas/Admin/Controllers/AdminHomeController.cs
+++ BOOKSTORE/Areas/Admin/Controllers/AdminHomeController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BOOKSTORE.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BOOKSTORE.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminHomeController : Controller
     {
+       
+        private readonly QlbhContext _context;
+
+        public AdminHomeController(QlbhContext context)
+        {
+            _context = context;
+        }
+        [Route("Admin/")]
+        [Route("Admin/AdminHome")]
+        [Route("Admin/index")]
         public IActionResult Index()
         {
             return View();
         }
+        
     }
 }
