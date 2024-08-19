3fce28d6fcf8fb0daaaa8623007dda877a29035f
HaMaxSit <vanha481@gmail.com>
2024-08-19 16:16:47 +0700

    cap nhat them du lieu class login va register

--- BOOKSTORE/Models/ErrorViewModel.cs
+++ BOOKSTORE/Models/ErrorViewModel.cs
@@ -6,4 +6,16 @@ namespace BOOKSTORE.Models
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
+    public class RegisterModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class LoginModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
 }
