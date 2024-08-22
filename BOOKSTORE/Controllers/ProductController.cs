6ab0620ad7cef4d7793f3aa4bea6af2726b95ae9
BUITHITUYETNGOC <buithituyetngoc2402@gmail.com>
2024-08-23 00:44:19 +0700

    Hiển thị chi tiết sản phẩm

---
+++ BOOKSTORE/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BOOKSTORE.Data;
+
+namespace BOOKSTORE.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly QlbhContext _context;
+
+        public ProductController(QlbhContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            var qlbhContext = _context.Products.Include(p => p.Category).Include(p => p.Supplier);
+            return View(await qlbhContext.ToListAsync());
+        }
+
+        // GET: Product/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.Id == id); // Sử dụng Id lấy details
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+    }
+}
