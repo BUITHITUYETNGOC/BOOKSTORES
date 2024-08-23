8d82061e8264d76e58b37c5f5baadb63bbe52af9
Nguyen Chau <hoangchau25102003@gmail.com>
2024-08-24 03:07:36 +0700

    fix giao diện cart

--- BOOKSTORE/Program.cs
+++ BOOKSTORE/Program.cs
@@ -3,6 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<QlbhContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyDb")));
@@ -21,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 
